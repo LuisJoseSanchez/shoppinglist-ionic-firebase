05cce5aa0d36a0b2fda2363cb6c64acf1b09a80d
Luis José Sánchez <luisjoseprofe@gmail.com>
2022-01-18 22:50:34 +0100

    Proyecto conectado a Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'shopping-list-c272a',
+    appId: '1:15033943625:web:cd0a90b0047ff3d2bae198',
+    storageBucket: 'shopping-list-c272a.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyB0AgwlMsYDStrXv-AB3scm10771IJKOpg',
+    authDomain: 'shopping-list-c272a.firebaseapp.com',
+    messagingSenderId: '15033943625',
+  },
   production: true
 };
