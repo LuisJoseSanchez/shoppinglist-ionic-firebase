2d461507b22968a205dcfe6b51d690361ce21492
Luis José Sánchez <luisjoseprofe@gmail.com>
2022-02-08 19:16:25 +0100

    Aplicación terminada funcionando bien

--- src/app/services/item.service.ts
+++ src/app/services/item.service.ts
@@ -4,34 +4,39 @@ import {
 } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import { Item } from '../model/item';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ItemService {
 
-  constructor(private firestore: Firestore) { }
+  pathToItems = `users/${this.auth.getCurrentUser().uid}/items`;
+
+  constructor(
+    private firestore: Firestore,
+    private auth: AuthService
+  ) { }
 
   public async addItem(item: Item) {
-    await addDoc(collection(this.firestore, 'items'), item);
+    await addDoc(collection(this.firestore, this.pathToItems), item);
   }
 
   getItems(): Observable<Item[]> {
-    const collectionRef = collection(this.firestore, 'items');
+    const collectionRef = collection(this.firestore, this.pathToItems);
     return collectionData(collectionRef, {idField: 'itemId'}) as Observable<Item[]>;
   }
 
   getItem(id: string): Observable<Item> {
-    const docRef = doc(this.firestore, `items/${id}`);
+    const docRef = doc(this.firestore, `${this.pathToItems}/${id}`);
     return docData(docRef, { idField: 'itemId' }) as Observable<Item>;
   }
 
   async deleteItem(id: string) {
-    await deleteDoc(doc(this.firestore, `items/${id}`));
+    await deleteDoc(doc(this.firestore, `${this.pathToItems}/${id}`));
   }
 
   async updateItem(item: Item) {
-    await setDoc(doc(this.firestore, `items/${item.itemId}`), item);
+    await setDoc(doc(this.firestore, `${this.pathToItems}/${item.itemId}`), item);
   }
-  
 }
